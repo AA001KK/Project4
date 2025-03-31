4355c3943f9a8419f170037d947cd242649ac52f
Kumush <kumushizziyeva@outlook.com>
2025-03-31 15:55:14 +0500

    ....

--- frontend/script.js
+++ frontend/script.js
@@ -1,44 +1,20 @@
-// Carousel harakatini dinamik qilish uchun
+let scrollAmount = 0;
+const scrollStep = 200;
 const carousel = document.querySelector('.carousel');
-const texts = document.querySelectorAll('.carousel p');
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
 
-let index = 0;
-
-// Matnlarni o‘ngga va chapga o'tkazish
-function updateCarousel() {
-  const offset = -index * texts[0].offsetWidth;
-  carousel.style.transform = `translateX(${offset}px)`;
+function scrollLeft() {
+  scrollAmount -= scrollStep;
+  if (scrollAmount < 0) {
+    scrollAmount = 0;
+  }
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
 }
 
-// O‘ng strelka bosilganda
-rightArrow.addEventListener('click', () => {
-  if (index < texts.length - 1) {
-    index++;
-  } else {
-    index = 0;
+function scrollRight() {  
+  scrollAmount += scrollStep;
+  if (scrollAmount > carousel.scrollWidth - carousel.clientWidth) {
+    scrollAmount = 0;
   }
-  updateCarousel();
-});
-
-// Chap strelka bosilganda
-leftArrow.addEventListener('click', () => {
-  if (index > 0) {
-    index--;
-  } else {
-    index = texts.length - 1;
-  }
-  updateCarousel();
-});
-
-// Avtomatik slayder funksiyasi
-setInterval(() => {
-  if (index < texts.length - 1) {
-    index++;
-  } else {
-    index = 0;
-  }
-  updateCarousel();
-}, 5000);
+  carousel.style.transform = `translateX(-${scrollAmount}px)`;
+}
 
